800636fec9e13013010e9291e36fb3c150cb9ac2
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-11-24 00:11:24 +0600

    update v1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6f5ec3cfa0948d0f9637ccde200386cd",
-"index.html": "fdf8f92360ce1fa5752f55a353863042",
-"/": "fdf8f92360ce1fa5752f55a353863042",
-"main.dart.js": "a4125147f21b0d9eabef119c9df7875a",
+"index.html": "a40413cb649009306d3043b045421423",
+"/": "a40413cb649009306d3043b045421423",
+"main.dart.js": "1132921b5e401d5a4c2a2b0b08d28461",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "1c580c7a2d994aeb55f240f5e2f08425",
 "assets/AssetManifest.json": "ebe8936473d5da09ddb0176fe8ea1b7a",
-"assets/NOTICES": "dd16cb63514ef7bc1b22f138f822e01a",
+"assets/NOTICES": "3fab81478c6a016b327e23b51a6a3401",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
